3c6715c2759438e1d272e9d47102bc8fd20a59b0
SanketG <sanketgawande.gcoey@gmail.com>
2022-02-21 10:25:23 +0530

    added navbar , responsive menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./navbar.scss";
 import { NAV_LINKS, SectionNavigation } from "./navTools";
+import { logo } from "./logo";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <nav>
       <div className="navbar">
         <div className="nav_logo">
-          <h4> sanket_g__</h4>
+          <img src={logo} alt="sanket" />
+        </div>
+        <div className="menu_icon" onClick={() => setToggle(true)}>
+          <img src="/humberger.svg" alt="menu" />
         </div>
         <ul className="navlinks">
           {NAV_LINKS.map((item) => {
@@ -20,11 +27,41 @@ const Navbar = () => {
                   onClick={() => SectionNavigation(item.yOffset)}
                 >
                   {item.link}
-                </NavLink>{" "}
+                </NavLink>
               </li>
             );
           })}
         </ul>
+        {toggle && (
+          <motion.ul
+            initial={{ x: "100vw" }}
+            animate={{ x: 0 }}
+            transition={{ duration: 0.5 }}
+            className="navlinks_sm"
+          >
+            <span className="toggle-cancel" onClick={() => setToggle(false)}>
+              <img src="/cancel.svg" alt="X" />
+            </span>
+
+            {NAV_LINKS.map((item) => {
+              return (
+                <li
+                  onClick={() => setToggle(false)}
+                  key={item.link}
+                  className="navlinks_item"
+                >
+                  <NavLink
+                    to={`/${item.link}`}
+                    className="navlinks_link"
+                    onClick={() => SectionNavigation(item.yOffset)}
+                  >
+                    {item.link}
+                  </NavLink>
+                </li>
+              );
+            })}
+          </motion.ul>
+        )}
       </div>
     </nav>
   );
